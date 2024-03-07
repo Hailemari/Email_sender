6a392268823ca88094312fa38471e8cf3185b307
Hailemari <hailemariamkefale19@gmail.com>
2024-03-07 09:52:35 +0300

    change the logs

--- app.js
+++ app.js
@@ -40,9 +40,9 @@ function getEmailContent(product_name, subscription_id, ended_at, created_at) {
 
     const differenceInMs = cancellationTime.getTime() - creationTime.getTime();
     const differenceInDays = Math.floor(differenceInMs / (1000 * 60 * 60 * 24));
-    console.log(cancellationTime.getTime())
-    console.log(creationTime.getTime())
-    console.log(daysDifference)
+    console.log(cancellationTime.getTime());
+    console.log(creationTime.getTime());
+    console.log(differenceInDays);
     if (differenceInDays < 7) {
         return `Dear Customer,\n\nYour subscription for ${product_name} (Subscription ID: ${subscription_id}) has been cancelled within the first seven days of subscription.\n\nIf you have any questions or concerns, please feel free to contact us.\n\nBest regards,\n`;
     } else if (differenceInDays < 30) {
