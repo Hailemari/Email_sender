4d0a840a0212c2758506fca9c6e5ef5f4007b2e3
Hailemari <hailemariamkefale19@gmail.com>
2024-03-12 21:29:19 +0300

    change message

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.post('/cancel_subscription', async (req, res) => {
     console.log(req.body);
 
     try {
-        const { user_email, subscription_id, product_name,created_at } = req.body;
+        const { user_email, subscription_id, product_name, created_at } = req.body;
 
         if (!user_email || !subscription_id || !product_name || !created_at) {
             return res.status(400).json({ error: 'Missing required fields in the request.' });
@@ -25,7 +25,7 @@ app.post('/cancel_subscription', async (req, res) => {
         const emailContent = getEmailContent(product_name, subscription_id, created_at);
         
 
-        await sendEmail(user_email, "Subscription Cancellation", emailContent);
+        await sendEmail(user_email, "Get Support & Claim Refund for your Bulk Mockup subscription?", emailContent);
         
         res.status(200).json({ message: 'Email sent to customer for subscription cancellation.' });
     } catch (error) {
@@ -44,13 +44,66 @@ function getEmailContent(product_name, subscription_id, created_at) {
     console.log(creationTime.getTime());
     console.log(differenceInDays);
 
+    let htmlContent = `<html><body><p>Subscription Cancelled!</p></body></html>`
+  
     if (differenceInDays < 7) {
-        return `Dear Customer,\n\nYour subscription for ${product_name} (Subscription ID: ${subscription_id}) has been cancelled within the first seven days of subscription.\n\nIf you have any questions or concerns, please feel free to contact us.\n\nBest regards,\n`;
+        htmlContent = `
+            <html>
+            <body>
+                <p>Hello {{ subscriber.first_name }},</p>
+                <p>I am Vikash, the creator of Bulk Mockup.</p>
+                <p>I noticed that you recently canceled your subscription within the first 7 days. Your satisfaction is our top priority.</p>
+                <p>I'd like to offer our quick support assistance to make sure you get the most out of it.</p>
+                <p>Would you be interested in scheduling a one-on-one support call at a time that suits you best?</p>
+                <p>During this call, I can address any concerns you may have, help you navigate and setup your workflow, and ensure you're getting the value you signed up for.</p>
+                <p><a href="https://cal.com/vikash-bulk-mockup/30min">Book a support call here</a></p>
+                <p>Best Regards</p>
+                <p>Vikash Kr. Prajapati</p>
+                <p>Founder Bulk Mockup</p>
+                <p>P.S. You are protected by our 7-day money-back guarantee. If you'd like to claim your refund, kindly <a href="https://forms.gle/1vKddypt6fTRFNmx6">fill up this form</a>.</p>
+            </body>
+            </html>
+        `;
     } else if (differenceInDays < 30) {
-        return `Dear Customer,\n\nYour subscription for ${product_name} (Subscription ID: ${subscription_id}) has been cancelled between 7 and 30 days of subscription.\n\nIf you have any  questions or concerns, please feel free to contact us.\n\nBest regards,\n`;
+        htmlContent = `
+            <html>
+            <body>
+                <p>Hello {{ subscriber.first_name }},</p>
+                <p>I am Vikash, the creator of Bulk Mockup.</p>
+                <p>I noticed that you recently canceled your subscription within the first 7 days. Your satisfaction is our top priority.</p>
+                <p>I'd like to offer our quick support assistance to make sure you get the most out of it.</p>
+                <p>Would you be interested in scheduling a one-on-one support call at a time that suits you best?</p>
+                <p>During this call, I can address any concerns you may have, help you navigate and setup your workflow, and ensure you're getting the value you signed up for.</p>
+                <p><a href="https://cal.com/vikash-bulk-mockup/30min">Book a support call here</a></p>
+                <p>Best Regards</p>
+                <p>Vikash Kr. Prajapati</p>
+                <p>Founder Bulk Mockup</p>
+                <p>P.S. You are protected by our 7-day money-back guarantee. If you'd like to claim your refund, kindly <a href="https://forms.gle/1vKddypt6fTRFNmx6">fill up this form</a>.</p>
+            </body>
+            </html>
+        `;
     } else {
-        return `Dear Customer,\n\nYour subscription for ${product_name} (Subscription ID: ${subscription_id}) has been cancelled after 30 days of subscription.\n\nIf you have any questions or concerns, please feel free to contact us.\n\nBest regards,\n`;
+        htmlContent = `
+            <html>
+            <body>
+                <p>Hello {{ subscriber.first_name }},</p>
+                <p>I am Vikash, the creator of Bulk Mockup.</p>
+                <p>I noticed that you recently canceled your subscription within the first 7 days. Your satisfaction is our top priority.</p>
+                <p>I'd like to offer our quick support assistance to make sure you get the most out of it.</p>
+                <p>Would you be interested in scheduling a one-on-one support call at a time that suits you best?</p>
+                <p>During this call, I can address any concerns you may have, help you navigate and setup your workflow, and ensure you're getting the value you signed up for.</p>
+                <p><a href="https://cal.com/vikash-bulk-mockup/30min">Book a support call here</a></p>
+                <p>Best Regards</p>
+                <p>Vikash Kr. Prajapati</p>
+                <p>Founder Bulk Mockup</p>
+                <p>P.S. You are protected by our 7-day money-back guarantee. If you'd like to claim your refund, kindly <a href="https://forms.gle/1vKddypt6fTRFNmx6">fill up this form</a>.</p>
+            </body>
+            </html>
+        `;
     }
+
+    return htmlContent;
+
 }
 
 async function sendEmail(receiverEmail, subject, message) {
@@ -70,7 +123,7 @@ async function sendEmail(receiverEmail, subject, message) {
             from: process.env.EMAIL_FROM,
             to: receiverEmail,
             subject: subject,
-            text: message
+            html: message
         };
 
         await transporter.sendMail(mailOptions);
